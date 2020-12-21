f66855e73f3c972bfc0d188e8bef9046a1bf5878
BeharBobbyGjidija <behargjidija@gmail.com>
2020-12-21 13:30:52 -0500

    Adding getRandomPhraseAsArray function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const btn__reset = document.getElementsByClassName('btn__reset');
-const missed = 0;
+const btn__reset = document.getElementsByClassName('btn__reset')[0]; // this will always return an HTML Collection
+let missed = 0;
 
-var phrases = ['This is my first phrase of the array',
+const phrases = ['This is my first phrase of the array',
             'This is my second phrase of the array',
             'This is my third Phrase of the array',
             'This is my forth Phrase of the array',
@@ -13,3 +13,10 @@ btn__reset.addEventListener('click', () => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
+
+function getRandomPhraseAsArray(arr) {
+let randomNumber = Math.floor(Math.random() * phrases.length);
+arr[randomNumber].indexOf =  arrayIndex;
+};
+
+getRandomPhraseAsArray(console.log(phrases));
